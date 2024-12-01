0acdb0231d545fda84004e3f9a8b288168be20ae
Aniket Mishra <mishraaniket949@gmail.com>
2024-12-02 00:30:33 +0530

    BASE_URL update

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = "https://rag-backend-78rfu0cfo-aniket-mishras-projects-85e5cb37.vercel.app/api";
 export const BASE_PY_URL = "http://localhost:5000";
