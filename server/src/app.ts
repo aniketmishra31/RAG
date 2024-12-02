9177610d7f0883d8c94fc4e66a9d898c3a840bf4
Aniket Mishra <mishraaniket949@gmail.com>
2024-12-02 13:38:51 +0530

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,13 +7,25 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: "https://rag-liard.vercel.app",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-apiKey"],
+    origin: true,
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization", "X-apiKey"]
+}));
+app.options("*", cors({
+    origin: true,
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization", "X-apiKey"]
 }));
 
-app.options("*", cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://rag-liard.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-apiKey');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 app.use(express.json());
 
